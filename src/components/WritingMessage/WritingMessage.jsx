b3069239b089f8cba15a55348b529523ca92ad29
DESKTOP-142GQLG\82106 <mangang0713@gmail.com>
2024-02-27 18:14:30 +0900

    Feat: textarea 제외한 폼 구성 완료

--- src/components/WritingMessage/WritingMessage.jsx
+++ src/components/WritingMessage/WritingMessage.jsx
@@ -1,12 +1,12 @@
-import WritingForm from "./WritingForm";
+// import WritingForm from './WritingForm';
 
-const WritingMessage = (props) => {
-  return (
-    <div>
-      <Header />
-      <WritingForm />
-    </div>
-  )
-}
+// const WritingMessage = (props) => {
+//   return (
+//     <div>
+//       <Header />
+//       <WritingForm />
+//     </div>
+//   )
+// }
 
-export default WritingMessage;
+// export default WritingMessage;
